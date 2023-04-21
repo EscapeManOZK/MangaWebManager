885b52d76f2db0d5a297409e1cef2edbf16e85b8
Romain OWCZAREK <romowczarek@gmail.com>
2023-04-21 17:43:32 +0200

    [MWA-1] Uniform project key

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,7 +1,7 @@
 import { Component} from '@angular/core';
 
 @Component({
-  selector: 'wma-welcome',
+  selector: 'mwa-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
