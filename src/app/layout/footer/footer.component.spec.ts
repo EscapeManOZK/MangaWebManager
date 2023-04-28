50e3ab62ac57f1f6f8fbfd4a8daff9152285bc81
Romain OWCZAREK <romowczarek@gmail.com>
2023-04-28 15:00:17 +0200

    [MWA-1] correct tests

--- src/app/layout/footer/footer.component.spec.ts
+++ src/app/layout/footer/footer.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,6 +9,7 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [NzLayoutModule],
       declarations: [ FooterComponent ]
     })
     .compileComponents();
--- src/app/layout/navbar/navbar.component.spec.ts
+++ src/app/layout/navbar/navbar.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,6 +13,7 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ TranslateModule.forRoot(), NzLayoutModule, NzDrawerModule, NzMenuModule, BrowserAnimationsModule ],
       declarations: [ NavbarComponent ]
     })
     .compileComponents();
